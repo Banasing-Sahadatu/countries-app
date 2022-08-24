fa6346a24f2b88916c17139defb637da1cbf140b
Banasing-Sahadatu <banasingsahadatu@gmail.com>
2022-08-24 17:30:42 +0000

    2nd Lesson on routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ContactScreen from "./screens/ContactScreen";
 import AboutScreen from "./screens/AboutScreen";
-import DetailScreen from "./screens/DetailScreen";
+import ProductScreen from "./screens/ProductScreen";
 import Header from "./components/Header";
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<HomeScreen />} />
         <Route path="/contact" element={<ContactScreen />} />
         <Route path="/about" element={<AboutScreen />} />
-        <Route path="/detail:name" element={<DetailScreen />} />
+        <Route path="/product" element={<ProductScreen />} />
         
       </Routes></div>
     </BrowserRouter>
